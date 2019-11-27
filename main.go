8228139549ca6df89248630e1b07e7634448a793
Hamish Hutchings <moredhel@aoeu.me>
2019-11-27 17:34:16 +0100

    add a better binary format

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
-	"github.com/moredhel/lagg/lagg"
 	"os"
+	"time"
+
+	"github.com/mkideal/cli"
+	"github.com/moredhel/lagg/lagg"
+
 )
 
+type argT struct {
+	cli.Helper
+	WindowSize  int    `cli:"windowsize" usage:"How many lines to output" dft:"16"`
+	MaxSize     int `cli:"maxsize" usage:"maximum number of logs to keep before we start compacting" dft:"1024"`
+	RefreshInterval  int    `cli:"refreshinterval" usage:"refresh interval of the output (ms)" dft:"100"`
+}
+
 func main() {
-	p := lagg.NewDefaultStream()
+	os.Exit(cli.Run(new(argT), func(ctx *cli.Context) error {
+		argv := ctx.Argv().(*argT)
 
-	p.Start()
-	defer p.Stop()
+		refreshInterval := time.Millisecond * time.Duration(argv.RefreshInterval)
+		p := lagg.NewStream(os.Stdin, argv.WindowSize, argv.MaxSize, refreshInterval)
 
-	p.ParseStream(os.Stdin)
+		p.Start()
+		defer p.Stop()
+
+		p.ParseStream(os.Stdin)
+		return nil
+	}))
 }
